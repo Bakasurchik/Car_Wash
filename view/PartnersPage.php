b72915c772998893cadb10c574037e78d9e15f1d
litresov <44399362+litresov@users.noreply.github.com>
2022-01-21 02:17:09 +0500

    ЛКЧШЕЕ ОБЮНОВЛЕНИЕ В ВАШЕЙ ЖИЗНИ

--- view/PartnersPage.php
+++ view/PartnersPage.php
@@ -2,48 +2,86 @@
     <head>
         <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
         <link rel="stylesheet" href="/css/style.css">
-        <script src="/js/ajax.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="/css/slider_style.css">
+        
     </head>
-    
-    <body>
-        <?php require_once dirname(__DIR__).'/view/Header.php'?>
-        <!-- Team -->
-        <main>
-            <div class="team" id="team">
-                <div class="container">
-                    <div class="team__inner">
+    <div class="wrapper">
+        <body>
+            <div class="lds-ring"></div>
+            <header>
+                <h1>
+                    Бронирование очереди на автомойку    
+                </h1>        
+            </header>
+            <?php require_once dirname(__DIR__).'/view/Header.php'?>
 
-                        <div class="team__item">
-                            <img class="team__photo" src="img/team/team-1.jpg" alt="">
-                    
-                            </div>
+            <main>
+            
+            <div class="carousel">
+                <div class="carousel__body">
+                    <div class="carousel__prev"><i class="far fa-angle-left"></i></div>
+                    <div class="carousel__next"><i class="far fa-angle-right"></i></div>
+                    <div class="carousel__slider">
+                    <div class="carousel__slider__item">
+                        <div class="item__3d-frame">
+                        <div class="item__3d-frame__box item__3d-frame__box--front">
+                            <img src="/img/Артем.jpg" width="435" height="500">
                         </div>
-                        <div class="team__item">
-                            <img class="team__photo" src="img/team/team-2.jpg" alt="">
-                    
-                            </div>
+                        <div class="item__3d-frame__box item__3d-frame__box--left"></div>
+                        <div class="item__3d-frame__box item__3d-frame__box--right">             </div>
                         </div>
-                        <div class="team__item">
-                            <img class="team__photo" src="img/team/team-3.jpg" alt="">
-                    
-                            </div>
+                    </div>
+                    <div class="carousel__slider__item">
+                        <div class="item__3d-frame">
+                        <div class="item__3d-frame__box item__3d-frame__box--front">
+                            <img src="https://c.tenor.com/72Xc7KpoOm4AAAAd/fiendish-swag-shadow-fiend.gif" width="435" height="500" />
                         </div>
-                        <div class="team__item">
-                            <img class="team__photo" src="img/team/team-4.jpg" alt="">
-                    
-                            </div>
+                        <div class="item__3d-frame__box item__3d-frame__box--left"></div>
+                        <div class="item__3d-frame__box item__3d-frame__box--right">             </div>
                         </div>
-                    <div class="team__item">
-                            <img class="team__photo" src="img/team/team-5.jpg" alt="">
-                    
-                            </div>
-                        </div>						
-                    </div><!-- /.team__inner -->
-                </div><!-- /.container -->
-            </div><!-- /.team -->
-        </main>
-        <footer>
-            <?php require_once dirname(__DIR__).'/view/Footer.php'?>
-        </footer>
-    </body>
+                    </div>
+                    <div class="carousel__slider__item">
+                        <div class="item__3d-frame">
+                        <div class="item__3d-frame__box item__3d-frame__box--front">
+                            <video width="500" height="500" preload="auto" muted="muted" autoplay="autoplay" loop="loop" >
+                                <source src="/img/Андрей_вид.mp4">
+                            </video>
+                        </div>
+                        <div class="item__3d-frame__box item__3d-frame__box--left"></div>
+                        <div class="item__3d-frame__box item__3d-frame__box--right">             </div>
+                        </div>
+                    </div>
+                    <div class="carousel__slider__item">
+                        <div class="item__3d-frame">
+                        <div class="item__3d-frame__box item__3d-frame__box--front">
+                            <img src="/img/Кирилл.jpg" width="435" height="500">
+                        </div>
+                        <div class="item__3d-frame__box item__3d-frame__box--left"></div>
+                        <div class="item__3d-frame__box item__3d-frame__box--right">             </div>
+                        </div>
+                    </div>
+                    <div class="carousel__slider__item">
+                        <div class="item__3d-frame">
+                        <div class="item__3d-frame__box item__3d-frame__box--front">
+                            <img src="/img/Дима.jpg" width="435" height="500">
+                        </div>
+                        <div class="item__3d-frame__box item__3d-frame__box--left"></div>
+                        <div class="item__3d-frame__box item__3d-frame__box--right">             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="OTSTUP3">
+            </div>       
+                
+            </main>
+                
+            <footer >
+                <?php require_once dirname(__DIR__).'/view/Footer.php'?>
+                <script src="/js/slider.js" type="text/javascript"></script>
+            </footer>
+        </body>
+    </div>
 </html>
+
+
